a69f6d093af9679b80d5930d0ea43f3b35293f85
anant <anant.gupta@utexas.edu>
2025-02-28 00:22:01 -0800

    Added code for tracer integration to generator

--- src/workflows/gemini_qa.py
+++ src/workflows/gemini_qa.py
@@ -1,4 +1,5 @@
 from adalflow.core import Component, Generator
+from adalflow.tracing import trace_generator_call, trace_generator_states
 
 from src.config.constants import GEMINI_20_FLASH
 from src.models.gemini_client import GeminiClient
@@ -6,7 +7,8 @@ from src.utils.config_loader import Config
 
 template_doc = r"""<SYS> You are a doctor </SYS> User: {{input_str}}"""
 
-
+@trace_generator_states()
+@trace_generator_call(error_only=False)
 class GeminiQA(Component):
     def __init__(self):
         super().__init__()
