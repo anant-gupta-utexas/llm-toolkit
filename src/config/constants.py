83f5ea3bf994517b7b88ac6e26bbf9464d3b1035
anant <anant.gupta@utexas.edu>
2025-05-08 23:13:33 -0700

    added constants for agents, tools and chains

--- src/config/constants.py
+++ src/config/constants.py
@@ -2,3 +2,15 @@
 GEMINI_15_FLASH = "GEMINI_1.5_FLASH"
 GEMINI_20_FLASH = "GEMINI_2.0_FLASH"
 LLAMA_32 = "OLLAMA_LLAMA3.2"
+
+# Tool Keys
+TOOL_MULTIPLY = "tool_multiply_v1"
+TOOL_RANDOM_NUMBER = "tool_generate_random_number_v1"
+
+# Agent Keys
+AGENT_TOOL_ASSISTANT = "agent_tool_assistant_v1"
+AGENT_QA_RESPONDER = "agent_qa_responder_v1"
+
+# Chain Keys
+CHAIN_TOOL_AGENT_WORKFLOW = "chain_tool_agent_workflow_v1"
+CHAIN_DIRECT_TOOL_EXECUTION = "chain_direct_tool_execution_v1"
